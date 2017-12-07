bcf6dfe73f534617c6685f5ac980d6d1ed0f214f
David Kim <davidjkim810@gmail.com>
2017-12-07 21:59:04 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move? (board, index)
   if board[index] != "X" && board[index] != "O"
   else def position_taken? (board, index)
-    board[index] == "X" && board[index] == "O"
+    board[index] == "X" && board[index] == "O" || board[index] == "X" || board[index] == "O"
   end
   end
 end
