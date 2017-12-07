fa27d4359d1c32afdf344ca1edd9623551d84f85
David Kim <davidjkim810@gmail.com>
2017-12-07 21:43:53 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move? (board, index)
-  board[index] != "X" && board[index] != "O"
+  board[index] != "X" && board[index] != "O" || board[index] != "X" || board[index] != "O"
 end
 
 
